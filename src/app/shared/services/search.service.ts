af968556b0f06770d18946dd9e1eb9a7dd4f8bd4
RUSLAN BOLKVADZE <ruslan@shopz.com>
2020-10-18 15:35:32 +0300

    Search request handler

--- src/app/shared/services/search.service.ts
+++ src/app/shared/services/search.service.ts
@@ -3,19 +3,37 @@ import { HttpClient } from '@angular/common/http';
 
 import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
+import { Movie } from '../models/search.interface';
 
+export interface SearchResponse {
+  Response: string;
+  Search?: Movie[];
+  totalResults?: string;
+  Error?: string;
+}
 @Injectable({
   providedIn: 'root',
 })
 export class SearchService {
   private API_URL = 'http://www.omdbapi.com/';
-  private API_KEY = 'f79aeba3';
+  private API_TOKEN = 'f79aeba3';
 
   constructor(private http: HttpClient) {}
 
   getMovies(query: string): Observable<any> {
-    const url = `${this.API_URL}?${this.API_KEY}&s=${query}`;
+    const url = `${this.API_URL}?apikey=${this.API_TOKEN}&s=${query}`;
 
-    return this.http.get(url).pipe(map((response: any) => response.items));
+    return this.http.get(url).pipe(
+      map((response: SearchResponse) => {
+        if (response.Response === 'True') {
+          return {
+            Search: response.Search,
+            totalResults: response.totalResults,
+          };
+        } else {
+          return { Search: 0 };
+        }
+      })
+    );
   }
 }
