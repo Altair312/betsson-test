b8d1a1bbd8d7fde81ef8fa9a800b41dac545b0bc
RUSLAN BOLKVADZE <ruslan@shopz.com>
2020-10-18 15:40:59 +0300

    Added components to the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,24 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
-
-import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { reducers, metaReducers } from './reducers';
 
+import { AppComponent } from './app.component';
+import { SearchContainerComponent } from './search/container/search-container/search-container.component';
+import { SearchInputComponent } from './search/components/search-input/search-input.component';
+import { SearchListComponent } from './search/components/search-list/search-list.component';
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    SearchContainerComponent,
+    SearchInputComponent,
+    SearchListComponent,
+  ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     StoreModule.forRoot({}, {}),
     StoreModule.forRoot(reducers, {
