03fedc0afb1a14fc18a3ab1c216ea111d90909a2
Ruslan Bolkvadze <ruslan@shopz.com>
2020-10-20 15:32:34 +0300

    Writing action logic

--- src/app/actions/search.actions.ts
+++ src/app/actions/search.actions.ts
@@ -1,4 +1,15 @@
-import { createAction} from "@ngrx/store";
+import { Action, createAction, props} from "@ngrx/store";
+import { Movie } from '../shared/models/search.interface';
 
-export const getSearchResults = createAction('[Search Component] Get Movies');
+export const enum MovieActionTypes {
+  GET_MOVIES = "[Movie] Get Movie List",
+  GET_SINGLE_MOVIE = "[Movie] Get Single Movie",
+  GET_MOVIES_SUCCESS = '[Movie] Get Movie List Success'
+}
 
+export const GetMovies = createAction(
+  MovieActionTypes.GET_MOVIES,
+  props<{payload: string}>(),
+)
+
+export const GetMoviesSuccess = createAction(MovieActionTypes.GET_MOVIES_SUCCESS, props<{payload: Movie[]}>())
