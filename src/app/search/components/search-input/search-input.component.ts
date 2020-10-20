0aae6c78379db56fc092e5263333719d848f5749
RUSLAN BOLKVADZE <ruslan@shopz.com>
2020-10-20 23:10:49 +0300

    Cleanup

--- src/app/search/components/search-input/search-input.component.ts
+++ src/app/search/components/search-input/search-input.component.ts
@@ -20,17 +20,19 @@ export class SearchInputComponent implements AfterViewInit {
   @Output() search: EventEmitter<string> = new EventEmitter<string>();
 
   constructor() {}
-  
+
   value = '';
-  
+
   onKeyUp(value: string) {
     this.value = value;
   }
 
-  
   ngAfterViewInit(): void {
-    const inputSubmit = fromEvent(this.inputElement.nativeElement, "keyup").pipe(filter((e: KeyboardEvent) => e.key === "Enter"));
-    const buttonSubmit = fromEvent(this.buttonElement.nativeElement, "click");
+    const inputSubmit = fromEvent(
+      this.inputElement.nativeElement,
+      'keyup'
+    ).pipe(filter((e: KeyboardEvent) => e.key === 'Enter'));
+    const buttonSubmit = fromEvent(this.buttonElement.nativeElement, 'click');
 
     const submit = merge(inputSubmit, buttonSubmit);
 
