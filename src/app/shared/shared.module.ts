9cbe3d82d097a2f9f784c6787f0877420b17ab75
Ruslan Bolkvadze <ruslan@shopz.com>
2020-10-21 16:43:56 +0300

    Cleanup

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BackButtonComponent } from './components/back-button/back-button.component';
 import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [BackButtonComponent],
   imports: [
     CommonModule,
     MatButtonModule,
+    MatProgressSpinnerModule
   ],
   exports: [
     BackButtonComponent,
+    MatButtonModule,
+    MatProgressSpinnerModule,
   ]
 })
 export class SharedModule { }
--- src/app/views/movie/movie.module.ts
+++ src/app/views/movie/movie.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { MovieContainerComponent } from './container/movie-view/movie-container.component';
 import { MovieCardComponent } from './component/movie-card/movie-card.component';
+
 import { SharedModule } from 'src/app/shared/shared.module';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,6 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
   imports: [
     CommonModule,
     SharedModule,
-    MatProgressSpinnerModule,
   ],
   exports: [
     MovieCardComponent,
